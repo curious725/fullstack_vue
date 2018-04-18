37fcc850fae0d3b38232d66d562f1a4972d28ddc
curious725 <curious725@gmail.com>
2018-04-18 18:25:29 +0300

    update template in CalendarEntry.vue component to show title of Active Day

--- calendar_app/src/app/components/CalendarEntry.vue
+++ calendar_app/src/app/components/CalendarEntry.vue
@@ -2,7 +2,9 @@
 <div id="calendar-entry">
   <div class="calendar-entry-note">
     <input type="text" placeholder="New Event" />
-    <p class="calendar-entry-day">Day of event: <span class="bold">Monday</span></p>
+    <p class="calendar-entry-day">Day of event: 
+      <span class="bold">{{ titleOfActiveDay }}</span>
+    </p>
     <a class="button is-primary is-small is-outlined">Submit</a>
   </div>
 </div>
