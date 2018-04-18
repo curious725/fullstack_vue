8d2ac910333914f913b2fc82c95864f39494da18
curious725 <curious725@gmail.com>
2018-04-18 17:13:18 +0300

    move scss for event from CalendarDay.vue to CalendarEvent.vue component

--- calendar_app/src/app/components/CalendarDay.vue
+++ calendar_app/src/app/components/CalendarDay.vue
@@ -49,28 +49,6 @@ export default {
   .day-details {
     padding: 10px;
 
-    .day-event {
-      margin-top: 6px;
-      margin-bottom: 6px;
-      display: block;
-      color: #4C4C4C;
-      padding: 5px;
-
-      .details {
-        display: block;
-      }
-
-      input {
-        background: none;
-        border: 0;
-        border-bottom: 1px solid #FFF;
-        width: 100%;
-
-        &:focus {
-          outline: none;
-        }
-      }
-    }
   }
 
   &:last-child {
--- calendar_app/src/app/components/CalendarEvent.vue
+++ calendar_app/src/app/components/CalendarEvent.vue
@@ -16,3 +16,28 @@ export default {
   props: ['event', 'day']
 }
 </script>
+
+<style lang="scss" scoped>
+.day-event {
+  margin-top: 6px;
+  margin-bottom: 6px;
+  display: block;
+  color: #4C4C4C;
+  padding: 5px;
+
+  .details {
+    display: block;
+  }
+
+  input {
+    background: none;
+    border: 0;
+    border-bottom: 1px solid #FFF;
+    width: 100%;
+
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
