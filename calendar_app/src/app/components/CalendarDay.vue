8a0c5f2d00479fc9f386914e517b93195e8fd5a6
curious725 <curious725@gmail.com>
2018-04-18 16:34:48 +0300

    in CalendarDay.vue add template and display day.abbvTitle and day.id

--- calendar_app/src/app/components/CalendarDay.vue
+++ calendar_app/src/app/components/CalendarDay.vue
@@ -1,3 +1,21 @@
+<template>
+  <div class="day column">
+    <div class="day-banner has-text-centered">{{ day.abbvTitle }}</div>
+    <div class="day-details">
+      <div class="day-number">{{ day.id }}</div>
+      <div class="day-event" style="background-color: rgb(153, 255, 153)">
+        <div>
+          <span class="has-text-centered details">Get Groceries</span> 
+          <div class="has-text-centered icons">
+            <i class="fa fa-pencil-square edit-icon"></i>
+            <i class="fa fa-trash-o delete-icon"></i>
+          </div>
+        </div>
+      </div>      
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
   name: 'CalendarDay',
