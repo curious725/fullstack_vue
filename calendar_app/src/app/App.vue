d406aeb55861c16fac8762c4526b64ee6b2685de
curious725 <curious725@gmail.com>
2018-04-18 13:27:26 +0300

    move from App.vue relevant markup to template of CalendarEntry.vue component

--- calendar_app/src/app/App.vue
+++ calendar_app/src/app/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    <div id="calendar-entry">
-      <div class="calendar-entry-note">
-        <input type="text" placeholder="New Event" />
-        <p class="calendar-entry-day">Day of event: <span class="bold">Monday</span></p>
-        <a class="button is-primary is-small is-outlined">Submit</a>
-      </div>
-    </div>
+
   </div>
 </template>
 
---
+++ calendar_app/src/app/components/CalendarEntry.vue
@@ -0,0 +1,9 @@
+<template>
+<div id="calendar-entry">
+  <div class="calendar-entry-note">
+    <input type="text" placeholder="New Event" />
+    <p class="calendar-entry-day">Day of event: <span class="bold">Monday</span></p>
+    <a class="button is-primary is-small is-outlined">Submit</a>
+  </div>
+</div>
+</template>
