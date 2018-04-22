fcef427bdc24f83e45cada9224b722c3deac4255
curious725 <curious725@gmail.com>
2018-04-22 13:20:57 +0300

    in CalendarEvent.vue add @click listener to delete icon

--- calendar_app/src/app/components/CalendarEvent.vue
+++ calendar_app/src/app/components/CalendarEvent.vue
@@ -5,7 +5,9 @@
     <div class="has-text-centered icons">
       <i class="fa fa-pencil-square edit-icon"
         @click="editEvent(day.id, event.details)"></i>
-      <i class="fa fa-trash-o delete-icon"></i>
+      <i class="fa fa-trash-o delete-icon"
+        @click="deleteEvent(day.id, event.details)">
+      </i>
     </div>
   </div>
   <div v-if="event.edit">
