594ee7a58b4c49aca3191f0094126de6b2461285
curious725 <curious725@gmail.com>
2018-04-22 11:35:29 +0300

    in CalendarEvent.vue display div only if event is not edited

--- calendar_app/src/app/components/CalendarEvent.vue
+++ calendar_app/src/app/components/CalendarEvent.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="day-event" :style="getEventBackgroundColor">
-  <div>
-    <span class="has-text-centered details">{{ event.details }}</span> 
+  <div v-if="!event.edit">
+    <span class="has-text-centered details">{{ event.details }}</span>
     <div class="has-text-centered icons">
       <i class="fa fa-pencil-square edit-icon"></i>
       <i class="fa fa-trash-o delete-icon"></i>
     </div>
   </div>
-</div>   
+</div>
 </template>
 
 <script>
